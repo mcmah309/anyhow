e4ad273eb8bbc580de67a701cb5b7999674f6a5c
mcmah309 <dillonmcmahon79@gmail.com>
2023-12-01 18:28:44 -0500

    feat: Migrate anyhow core features out into the rust_core package

--- lib/src/base/unit.dart
+++
@@ -1,9 +0,0 @@
-import '../../base.dart';
-
-/// Type alias for (). Used for a [Result] when the returned value does not matter. Preferred over void since
-/// forces stricter types. See [unit], [okay], and [error]
-typedef Unit = ();
-
-const unit = const ();
-const okay = const Ok(unit);
-const error = const Err(unit);
