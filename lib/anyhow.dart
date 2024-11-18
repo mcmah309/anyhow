21247d2b3fc45366d2050afe2a56190e0ff6b04b
mcmah309 <dillonmcmahon79@gmail.com>
2024-11-18 22:46:17 +0000

    feat: Export FutureResultExtension

--- lib/anyhow.dart
+++ lib/anyhow.dart
@@ -4,5 +4,6 @@ library anyhow;
 export 'src/error.dart';
 export 'src/guard.dart';
 
+export 'package:rust/src/result/result.dart' show FutureResultExtension;
 export 'package:rust/src/result/record_to_result_extensions.dart';
 export 'package:rust/src/result/result_extensions.dart';
